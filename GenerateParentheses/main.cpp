a44cf0906ef550a8bb694238e9e692285ac50c3b
stevyz <zhaofyyufei@hotmail.com>
2016-06-08 18:22:04 -0400

    rec solution v1

--- GenerateParentheses/main.cpp
+++ GenerateParentheses/main.cpp
@@ -1,23 +1,9 @@
-// next_permutation example
+// version 2, recursive solution
 #include <iostream>     // std::cout
-#include <algorithm>    // std::next_permutation, std::sort
 #include <vector>
 #include <string>
 
-/*bool verify(std::vector<signed char> v)
-{
-	int counter = 0;
-	for (auto i = v.begin(); i != v.end(); ++i)
-	{
-
-		counter += *i;
-		if (counter < 0)
-		{
-			return false;
-		}
-	}
-	return true;
-}*/
+int n;
 
 template <typename T>
 int printVector(std::vector<T> v)
@@ -29,52 +15,42 @@ int printVector(std::vector<T> v)
 	return 0;
 }
 
-int main() {
-	int n = 10;
-	std::vector<std::string> result;
-	std::vector<signed char> target(2*n, 1);
-	for (int i = 0; i < n; i++)
+int helper(signed char k, std::string str, std::vector<std::string>& result, int L)
+{
+	//std::cout << k << str << L;
+
+	if (str.length() == 2 * n)
 	{
-		target[i]=-1;
-		//std::cout << target.size() << "/" << target.capacity();
+		result.push_back(str);
+		return 0;
 	}
-
-	do {		
-		bool veri = true;
-
-		int counter = 0;
-
-		for (auto i = target.begin(); i != target.end(); ++i)
+	else 
+	{
+		if (k > 0)
 		{
-			counter += *i;
-			if (counter < 0)
-			{
-				veri =  false;
-				break;
-			}
+			//std::cout << k << str;
+			helper(k-1, str + ")", result, L);
+		}
+		if (L < n) 
+		{
+			//std::cout << k<< str;
+			helper(k+1, str + "(", result, L+1);
 		}
 
-		if (veri)
-		{
-			std::string s="";
-			for (auto i = target.begin(); i != target.end(); ++i)
-			{
-				if (*i==-1)
-				{
-					s += ")";
-				}
-				if (*i == 1)
-				{
-					s += "(";
-				}
 
-			}
-			result.push_back(s);			
-		}
-		
-	} while (std::next_permutation(target.begin(), target.end()));
-	printVector(result);
+	}
+	return 0;
+
+}
+
+int main() {
+	n = 10;
+	std::vector<std::string> result;
+	helper(0, "", result, 0);
+	//printVector(result);
 	return 0;
 }
+
+
 
 
