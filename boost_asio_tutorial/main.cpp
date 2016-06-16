c3b82b1605b0cf9b80da6fe2e77a38c902244d77
yfzhao <zhaofei.steven@gmail.com>
2016-06-16 00:01:55 -0400

    Complete client side.

--- boost_asio_tutorial/main.cpp
+++ boost_asio_tutorial/main.cpp
@@ -19,6 +19,27 @@ int main(int argc, char* argv[])
 		tcp::resolver::query query(argv[1], "daytime");
 		tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 
+		tcp::socket socket(io_service);
+		boost::asio::connect(socket, endpoint_iterator);
+
+		for (;;)
+		{
+			std::array<char, 128> buf;
+			boost::system::error_code error;
+			size_t len = socket.read_some(boost::asio::buffer(buf), error);
+			if (error == boost::asio::error::eof)
+			{
+				break;
+			}
+			else if (error)
+			{
+				throw boost::system::system_error(error);
+			}
+
+			std::cout.write(buf.data(), len);
+		}
+
+
 	}
 	catch (std::exception& e)
 	{
