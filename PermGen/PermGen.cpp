f1b683ed44003aab634df3a7331463e94247414b
yfzhao <zhaofei.steven@gmail.com>
2016-07-06 00:04:38 -0400

    Init commit (in progress)

---
+++ PermGen/PermGen.cpp
@@ -0,0 +1,17 @@
+// Non-recursive Heap's method
+// R.Sedgewick 1977
+
+int main() {
+	
+	char perm[3] = {'1', '2', '3'};
+	const int N = sizeof(perm)/sizeof(perm[0]);
+	int c[N];
+	int i{ N - 1};
+	do {
+		c[i] = 1;
+	} while (i > 1, i--);
+
+
+
+	return 0;
+}
